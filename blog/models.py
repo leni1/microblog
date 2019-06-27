03f6cab006349c8192846fbb644fedd798980418
Leni Kadali <lenikmutungi@gmail.com>
2019-06-27 13:58:35 +0300

    ft: Add followers association table
    Added the followers association table to track user followers
    in the application. Since it is an auxiliary table that has
    no data other than foreign keys, it does not need an associated
    model class.

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,13 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from blog import db, login
 
 
+followers = db.Table(
+    'followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
