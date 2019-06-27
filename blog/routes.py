dea3f69c4b9f54334083d82bf28095a1494195da
Leni Kadali <lenikmutungi@gmail.com>
2019-06-27 20:29:55 +0300

    ft: Remove fake posts, replace with posts from followed users
    Removed fake posts and replaced them with posts from the users
    the current user is following.

--- blog/routes.py
+++ blog/routes.py
@@ -29,16 +29,7 @@ def index():
         db.session.commit()
         flash('Your post is now live!')
         return redirect(url_for('index'))
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Kampala!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Matrix is a really cool movie!'
-        }
-    ]
+    posts = current_user.followed_posts().all()
     return render_template(
             'index.html',
             title='Home Page',
