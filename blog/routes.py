f11bd7002eebc79e37fde61e476dae5661998a7d
Leni Kadali <lenikmutungi@gmail.com>
2019-06-28 19:30:01 +0300

    Add reset password view function

--- blog/routes.py
+++ blog/routes.py
@@ -5,9 +5,11 @@ from flask_login import current_user, login_required, login_user, logout_user
 from werkzeug.urls import url_parse
 
 from blog import app, db
+from blog.email import send_password_reset_email
 from blog.forms import (
         EditProfileForm, LoginForm,
-        PostForm, RegistrationForm)
+        PostForm, RegistrationForm,
+        ResetPasswordRequestForm)
 from blog.models import Post, User
 
 
@@ -78,6 +80,21 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/reset_password_request', methods=['GET', 'POST'])
+def reset_password_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = ResetPasswordRequestForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            send_password_reset_email(user)
+        flash('Check your email for the instructions to reset your password')
+        return redirect(url_for('login'))
+    return render_template('reset_password_request.html',
+                           title='Reset Password', form=form)
 
 
 @app.route('/user/<username>')
