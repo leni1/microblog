a1f7a393dafd3e5f46e029ab18f8b669d6f91326
Leni Kadali <lenikmutungi@gmail.com>
2019-07-01 21:49:10 +0300

    Add function for generation of user tokens

--- blog/api/tokens.py
+++ blog/api/tokens.py
@@ -1,5 +1,15 @@
+from flask import jsonify, g
+from blog import db
+from blog.api import bp
+from blog.api.auth import basic_auth
+
+
+@bp.route('/tokens', methods=['POST'])
+@basic_auth.login_required
 def get_token():
-    pass
+    token = g.current_user.get_token()
+    db.session.commit()
+    return jsonify({'token': token})
 
 
 def revoke_token():
