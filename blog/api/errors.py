ea6e893153b84ae71bc779e076be8c589c3ccc5e
Leni Kadali <lenikmutungi@gmail.com>
2019-07-01 11:30:24 +0300

    Add Bad request responses

--- blog/api/errors.py
+++ blog/api/errors.py
@@ -9,3 +9,7 @@ def error_response(status_code, message=None):
     response = jsonify(payload)
     response.status_code = status_code
     return response
+
+
+def bad_request(message):
+    return error_response(400, message)
