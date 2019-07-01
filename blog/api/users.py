cde2793e3daf51c71837320fe85ebbfe03367dc9
Leni Kadali <lenikmutungi@gmail.com>
2019-07-01 11:52:31 +0300

    Add register user view function

--- blog/api/users.py
+++ blog/api/users.py
@@ -1,6 +1,8 @@
-from flask import jsonify, request
+from flask import jsonify, request, url_for
 
+from blog import db
 from blog.api import bp
+from blog.api.errors import bad_request
 from blog.models import User
 
 
@@ -39,7 +41,21 @@ def get_followed(id):
 
 @bp.route('/users', methods=['POST'])
 def create_user():
-    pass
+    data = request.get_json() or {}
+    if 'username' not in data or 'email' not in data or 'password' not in data:
+        return bad_request('Must include username, email and password fields.')
+    if User.query.filter_by(username=data['username']).first():
+        return bad_request('Please use a different username.')
+    if User.query.filter_by(email=data['email']).first():
+        return bad_request('Please use a different email address.')
+    user = User()
+    user.from_dict(data, new_user=True)
+    db.session.add(user)
+    db.session.commit()
+    response = jsonify(user.to_dict())
+    response.status_code = 201
+    response.headers['Location'] = url_for('api.get_user', id=user.id)
+    return response
 
 
 @bp.route('/users/<int:id>', methods=['PUT'])
