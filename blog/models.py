63cf0d63520096d8c88e9ca7e05c93904d1e40e8
Leni Kadali <lenikmutungi@gmail.com>
2019-07-01 10:15:23 +0300

    Add to_dict method that converts user model to JSON friendly format

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from time import time
 
 import jwt
 
+from flask import url_for
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -119,6 +120,26 @@ class User(UserMixin, db.Model):
         except:
             return
         return User.query.get(id)
+
+    def to_dict(self, include_email=False):
+        data = {
+            'id': self.id,
+            'username': self.username,
+            'last_seen': self.last_seen.isoformat() + 'Z',
+            'about_me': self.about_me,
+            'post_count': self.posts.count(),
+            'follower_count': self.followers.count(),
+            'followed_count': self.followed.count(),
+            '_links': {
+                'self': url_for('api.get_user', id=self.id),
+                'followers': url_for('api.get_followers', id=self.id),
+                'followed': url_for('api.get_followed', id=self.id),
+                'avatar': self.avatar(128)
+            }
+        }
+        if include_email:
+            data['email'] = self.email
+        return data
 
 
 @login.user_loader
